378cc2b115bf28b1aa2710571b5c108662769ec4
pasindu <bgpsandaruwan@gmail.com>
2020-06-06 09:03:19 +0530

    error soled

--- FrontEnd1/src/app/service/case.service.ts
+++ FrontEnd1/src/app/service/case.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Case} from '../model/Case';
-import {Client} from "../model/Client";
 
 const URL = '/CaseController';
 
