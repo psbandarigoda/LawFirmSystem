28c2e8a10b2add4b884d9af913ed24888e42b9ca
pasindu <bgpsandaruwan@gmail.com>
2020-10-21 05:22:11 +0530

    login page changed

--- FrontEnd1/src/app/view/login/login.component.ts
+++ FrontEnd1/src/app/view/login/login.component.ts
@@ -14,6 +14,8 @@ export class LoginComponent implements OnInit {
   returnUrlCla: string;
   uName: string;
   uPwd: string;
+  quote1: string = '"an act of the law';
+  quote2: string = 'does_no_one_wrong."';
 
   constructor(private route: ActivatedRoute,
               private formBuilder: FormBuilder,
@@ -24,6 +26,10 @@ export class LoginComponent implements OnInit {
       username: ['', Validators.required],
       password: ['', Validators.required]
     });
+    this.loginForm.reset();
+
+    setTimeout(()=>{ this.quote1 = '"If there were no bad people,'; this.quote2 = 'there_would_be_no_good_lawyers."';}, 4000)
+    setTimeout(()=>{ this.quote1 = '"an act of the law'; this.quote2 = 'does_no_one_wrong."';}, 8000)
 
     this.returnUrlLaw = this.route.snapshot.queryParams['returnUrlLaw'] || 'main/dashboard';
     this.returnUrlCla = this.route.snapshot.queryParams['returnUrlCla'] || 'clerkMain/clerk';
